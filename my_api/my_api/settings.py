8e3323f20e563739b08b154e5252c87e6c2530b4
Tequelz <sc17gt@leeds.ac.uk>
2021-04-22 19:55:38 +0100

    Settings with heroku

--- my_api/my_api/settings.py
+++ my_api/my_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,4 +144,4 @@ REST_FRAMEWORK = {
 
 SITE_ID = 1
 
-django_heroku.settings(local())
+django_heroku.settings(locals())
