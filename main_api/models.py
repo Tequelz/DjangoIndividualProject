b41db8fe7d19ca420c44c6e2d5a9d37667a78b65
Tequelz <sc17gt@leeds.ac.uk>
2021-04-29 16:25:40 +0100

    Fixing Teaching Session Unique

--- main_api/models.py
+++ main_api/models.py
@@ -27,7 +27,13 @@ class TeachingSession(models.Model):
     lesson_id = models.ForeignKey(LessonID,on_delete=models.CASCADE,default=0)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['username', 'lesson_id'], name='unique_sign_in'),
-        ]
+    def validate_unique(self,*args,**kwargs):
+        super().validate_unique(*args,**kwargs)
+        if self.__class__.objects.filter(username=self.username,lesson_id=self.lesson_id).exists():
+            return {"invalid":"not unique"}
+
+
+    # class Meta:
+    #     constraints = [
+    #         models.UniqueConstraint(fields=['username', 'lesson_id'], name='unique_sign_in'),
+    #     ]
