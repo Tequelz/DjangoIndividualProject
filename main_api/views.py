02d81d53cda7e28b1edaac195f979d0a05e0f17f
Tequelz <sc17gt@leeds.ac.uk>
2021-04-30 20:13:34 +0100

    New updated backend for creating modules, lectures, and sessions

--- main_api/views.py
+++ main_api/views.py
@@ -62,7 +62,7 @@ class LessonIDCheck(generics.ListCreateAPIView):
     serializer_class = LectureSerializer
 
     def post(self, request, *args, **kwargs):
-        qs = list(Lecture.objects.filter(lec_id= request.data.get("lec_id"),lec_number= request.data.get("lec_num"),lec_length=request.data.get("lec_len")).values_list('pk',flat=True))
+        qs = list(Lecture.objects.filter(lec_id= request.data.get("lec_id"),lec_number= request.data.get("lec_num"),lec_length=request.data.get("lec_len")).values('pk'))
 
         # serializer = LectureSerializer(qs, many=True)
         # data = serializer.data
