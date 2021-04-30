ee909e3eb7f57c1c995fd71cbc73f2bccf3ee0f4
Tequelz <sc17gt@leeds.ac.uk>
2021-04-30 12:56:06 +0100

    Filter by user present in teaching sesh

--- main_api/views.py
+++ main_api/views.py
@@ -75,7 +75,7 @@ class TeachingSessionCreateView(generics.ListCreateAPIView):
 
         serializer = TeachingSessionSerializer(qs,many=True)
         data = serializer.data
-        return data
+        return JsonResponse(data)
 
 
 
