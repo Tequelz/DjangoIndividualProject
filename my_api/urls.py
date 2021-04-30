3cd6089940be1ed6488fe8b942d448c812886501
Tequelz <sc17gt@leeds.ac.uk>
2021-04-30 19:56:55 +0100

    New updated backend for creating modules, lectures, and sessions

--- my_api/urls.py
+++ my_api/urls.py
@@ -4,13 +4,14 @@ from django.urls import path,include
 from rest_framework.authtoken.views import obtain_auth_token
 
 
-from main_api.views import GetUserDetails, ModuleCreateView, LectureSessionCreateView, LectureCreateView
+from main_api.views import GetUserDetails, ModuleCreateView, LectureSessionCreateView, LectureCreateView, LessonIDCheck
 
 urlpatterns = [
     path('api-auth/',include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('module-create/',ModuleCreateView.as_view(),name="create-module"),
     path('lecture-create/',LectureCreateView.as_view(),name="create-lecture"),
+    path('lecture-check/',LessonIDCheck.as_view(),name="id-check"),
     path('user-attend/',LectureSessionCreateView.as_view(),name="add-student"),
     path('get-user-by-id/',GetUserDetails.as_view(),name="get-user"),
     path('api/token/',obtain_auth_token,name='obtain-token'),
